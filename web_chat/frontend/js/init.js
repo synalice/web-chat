0997c818fea21151a8e8697235a2a8a956448985
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-04-30 16:49:32 +0300

    🎨 Remove init.js

--- web_chat/frontend/js/init.js
+++
@@ -1,5 +0,0 @@
-// Here I instantiate and run everything I need to be loaded on the page
-
-const message = new Message("#message-input", "http://127.0.0.1:8000/web-chat/posts/new");
-
-set_keyboard_shortcuts();
--- web_chat/frontend/js/main.js
+++ web_chat/frontend/js/main.js
@@ -1,4 +1,5 @@
-// Here is everything required for sending messages
+const message = new Message("#message-input", "http://127.0.0.1:8000/web-chat/posts/new");
+set_keyboard_shortcuts();
 
 function main() {
 	message.send_message();
